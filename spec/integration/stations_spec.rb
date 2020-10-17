07d556a1d692f2aa264dd77b4c48ce3afed29de0
Yuri Chandra Tri Putra <yurichandra@gmail.com>
2020-10-17 13:49:07 +0700

    docs: add API docs

--- spec/integration/stations_spec.rb
+++ spec/integration/stations_spec.rb
@@ -2,6 +2,22 @@ require 'swagger_helper'
 
 describe 'Stations API' do
   path '/stations' do
+    get 'Get all stations' do
+      tags 'Stations'
+      produces 'application/json'
+      parameter name: :includes, description: 'Resource that can be included on response. Available: province, city, district', in: :query, schema: {
+        type: :string,
+        properties: {
+          includes: { type: :string }
+        }
+      }
+      response '200', 'OK' do
+        run_test!
+      end
+    end
+  end
+
+  path '/stations' do
     post 'Create a station' do
       tags 'Stations'
       consumes 'application/json'
