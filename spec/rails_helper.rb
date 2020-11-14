3698b17f32bca02f85d71b1f158ecb1dad5357fe
Yuri Chandra <yurichandra@gmail.com>
2020-11-14 19:21:32 +0700

    test: move database_cleaner config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,20 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  DatabaseCleaner.clean_with(:truncation)
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
